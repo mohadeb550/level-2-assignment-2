7f709469e9b751cd5c52e03f97b8aa8c16eb5dbb
mohadebDev <maxff00789@gmail.com>
2024-05-20 21:32:39 +0600

    initialized project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+
+import app from "./app";
+import config from "./config";
+import mongoose from "mongoose";
+
+//  run server and connect with mongodb by mongoose 
+
+  async function main() {
+
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`)
+      })
+    
+    await mongoose.connect(config.db_url as string);
+  }
+
+  main()
